e509be39354abdf4b46e15e139504ad6fcea7a1d
mattwiebe <mattwiebe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-28 22:35:56 +0000

    bump version to 1.0.5
    git-svn-id: https://plugins.svn.wordpress.org/advanced-post-manager/trunk@445026 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tribe-apm.php
+++ tribe-apm.php
@@ -2,7 +2,7 @@
 /*
  Plugin Name:  Advanced Post Manager
  Description:  Dialing custom post types to 11 with advanced filtering controls.
- Version: 1.0.4
+ Version: 1.0.5
  Author: Modern Tribe, Inc.
  Author URI: http://tri.be/
  Text Domain: tribe-apm
